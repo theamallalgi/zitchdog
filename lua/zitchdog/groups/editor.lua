c255f06ac33335eac2a8e72a9304bea0d05312e2
Amal Lalgi <amallalgi2487@gmail.com>
2024-12-17 23:37:10 +0530

    refactor(hl): update highlight groups for alpha.nvim

--- lua/zitchdog/groups/editor.lua
+++ lua/zitchdog/groups/editor.lua
@@ -182,9 +182,9 @@ local function createEditorGroup(palette, zitch)
 		NoiceCmdlineIconSearch = { fg = palette.orange, bg = "NONE" },
 
 		AlphaHeader = { fg = palette.purple, bg = "NONE" },
-		AlphaButtonText = { fg = "#635e80", bg = "NONE", bold = true },
-		AlphaButtonShortcut = { fg = "#46425c", bg = "NONE", italic = false },
-		AlphaFooter = { fg = "#2f234b", bg = "NONE", italic = false },
+		AlphaButtonText = { fg = palette.purple, bg = "NONE", bold = true },
+		AlphaButtonShortcut = { fg = palette.mulberry, bg = "NONE", italic = false },
+		AlphaFooter = { fg = palette.mulberry, bg = "NONE", italic = false },
 	}
 end
 
