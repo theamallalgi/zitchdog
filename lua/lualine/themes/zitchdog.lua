7aac43ba7f0f365b941f25c9c5aa2c7bb109237b
Amal Lalgi <amallalgi2487@gmail.com>
2025-08-03 15:59:17 +0530

    fix: resolve lualine stuck with default theme issue #2

--- lua/lualine/themes/zitchdog.lua
+++ lua/lualine/themes/zitchdog.lua
@@ -1,5 +1,3 @@
-local M = {}
-
 local palettes = require("zitchdog.common.palette").variants
 
 local function get_palette()
@@ -17,37 +15,37 @@ end
 
 local colors = get_palette()
 
-M = {
-	normal = {
-		a = { fg = colors.black, bg = colors.purple, gui = "bold" },
-		b = { fg = colors.white, bg = colors.mauve },
-		c = { fg = colors.white, bg = colors.ash },
-	},
-	insert = {
-		a = { fg = colors.black, bg = colors.green, gui = "bold" },
-		b = { fg = colors.white, bg = colors.mauve },
-		c = { fg = colors.white, bg = colors.ash },
-	},
-	visual = {
-		a = { fg = colors.black, bg = colors.orange, gui = "bold" },
-		b = { fg = colors.white, bg = colors.mauve },
-		c = { fg = colors.white, bg = colors.ash },
-	},
-	replace = {
-		a = { fg = colors.black, bg = colors.coral, gui = "bold" },
-		b = { fg = colors.white, bg = colors.mauve },
-		c = { fg = colors.white, bg = colors.ash },
-	},
-	command = {
-		a = { fg = colors.black, bg = colors.cyan, gui = "bold" },
-		b = { fg = colors.white, bg = a.mauve },
-		c = { fg = colors.white, bg = colors.ash },
-	},
-	inactive = {
-		a = { fg = colors.white, bg = colors.ash, gui = "bold" },
-		b = { fg = colors.white, bg = colors.ash },
-		c = { fg = colors.white, bg = colors.ash },
-	},
+local theme = {
+  normal = {
+    a = { fg = colors.black, bg = colors.purple, gui = "bold" },
+    b = { fg = colors.white, bg = colors.mauve },
+    c = { fg = colors.white, bg = colors.ash },
+  },
+  insert = {
+    a = { fg = colors.black, bg = colors.green, gui = "bold" },
+    b = { fg = colors.white, bg = colors.mauve },
+    c = { fg = colors.white, bg = colors.ash },
+  },
+  visual = {
+    a = { fg = colors.black, bg = colors.orange, gui = "bold" },
+    b = { fg = colors.white, bg = colors.mauve },
+    c = { fg = colors.white, bg = colors.ash },
+  },
+  replace = {
+    a = { fg = colors.black, bg = colors.coral, gui = "bold" },
+    b = { fg = colors.white, bg = colors.mauve },
+    c = { fg = colors.white, bg = colors.ash },
+  },
+  command = {
+    a = { fg = colors.black, bg = colors.cyan, gui = "bold" },
+    b = { fg = colors.white, bg = colors.mauve },
+    c = { fg = colors.white, bg = colors.ash },
+  },
+  inactive = {
+    a = { fg = colors.white, bg = colors.ash, gui = "bold" },
+    b = { fg = colors.white, bg = colors.ash },
+    c = { fg = colors.white, bg = colors.ash },
+  },
 }
 
-return M
+return theme
