0b086c6c8e03667fa7b22e7cfb60fbd5ce87b961
Amal Lalgi <amallalgi2487@gmail.com>
2025-08-03 16:09:41 +0530

    fix: resolve lualine stuck with default theme issue #3

--- lua/lualine/themes/zitchdog.lua
+++ lua/lualine/themes/zitchdog.lua
@@ -1,51 +1,53 @@
-local palettes = require("zitchdog.common.palette").variants
+local M = {}
 
 local function get_palette()
   local current_colorscheme = vim.g.colors_name
+  local palettes = require("zitchdog.common.palette").variants
+
   if current_colorscheme == "zitchdog-dune" then
     return palettes.dune
   elseif current_colorscheme == "zitchdog-pine" then
     return palettes.pine
-  elseif current_colorscheme == "zitchdog-grape" then
-    return palettes.grape
   else
     return palettes.grape -- Default to grape
   end
 end
 
-local colors = get_palette()
+function M.get_theme()
+  local colors = get_palette()
 
-local theme = {
-  normal = {
-    a = { fg = colors.black, bg = colors.purple, gui = "bold" },
-    b = { fg = colors.white, bg = colors.mauve },
-    c = { fg = colors.white, bg = colors.ash },
-  },
-  insert = {
-    a = { fg = colors.black, bg = colors.green, gui = "bold" },
-    b = { fg = colors.white, bg = colors.mauve },
-    c = { fg = colors.white, bg = colors.ash },
-  },
-  visual = {
-    a = { fg = colors.black, bg = colors.orange, gui = "bold" },
-    b = { fg = colors.white, bg = colors.mauve },
-    c = { fg = colors.white, bg = colors.ash },
-  },
-  replace = {
-    a = { fg = colors.black, bg = colors.coral, gui = "bold" },
-    b = { fg = colors.white, bg = colors.mauve },
-    c = { fg = colors.white, bg = colors.ash },
-  },
-  command = {
-    a = { fg = colors.black, bg = colors.cyan, gui = "bold" },
-    b = { fg = colors.white, bg = colors.mauve },
-    c = { fg = colors.white, bg = colors.ash },
-  },
-  inactive = {
-    a = { fg = colors.white, bg = colors.ash, gui = "bold" },
-    b = { fg = colors.white, bg = colors.ash },
-    c = { fg = colors.white, bg = colors.ash },
-  },
-}
+  return {
+    normal = {
+      a = { fg = colors.black, bg = colors.purple, gui = "bold" },
+      b = { fg = colors.white, bg = colors.mauve },
+      c = { fg = colors.white, bg = colors.ash },
+    },
+    insert = {
+      a = { fg = colors.black, bg = colors.green, gui = "bold" },
+      b = { fg = colors.white, bg = colors.mauve },
+      c = { fg = colors.white, bg = colors.ash },
+    },
+    visual = {
+      a = { fg = colors.black, bg = colors.orange, gui = "bold" },
+      b = { fg = colors.white, bg = colors.mauve },
+      c = { fg = colors.white, bg = colors.ash },
+    },
+    replace = {
+      a = { fg = colors.black, bg = colors.coral, gui = "bold" },
+      b = { fg = colors.white, bg = colors.mauve },
+      c = { fg = colors.white, bg = colors.ash },
+    },
+    command = {
+      a = { fg = colors.black, bg = colors.cyan, gui = "bold" },
+      b = { fg = colors.white, bg = colors.mauve },
+      c = { fg = colors.white, bg = colors.ash },
+    },
+    inactive = {
+      a = { fg = colors.white, bg = colors.ash, gui = "bold" },
+      b = { fg = colors.white, bg = colors.ash },
+      c = { fg = colors.white, bg = colors.ash },
+    },
+  }
+end
 
-return theme
+return M
