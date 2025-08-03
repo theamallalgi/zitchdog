5bd3d5bd5af4a01e76669568e7d47448d5f56d73
Amal Lalgi <amallalgi2487@gmail.com>
2025-08-03 17:57:07 +0530

    test: why?

--- lua/lualine/themes/_zitchdog.lua
+++ lua/lualine/themes/_zitchdog.lua
@@ -10,33 +10,33 @@ function M.get(variant)
   local zitchdog = {
     normal = {
       a = { fg = colors.black, bg = colors.purple, gui = "bold" },
-      b = { fg = colors.white, bg = colors.mauve },
-      c = { fg = colors.white, bg = colors.darkblack },
+      b = { fg = colors.purple, bg = colors.mauve },
+      c = { fg = colors.purple, bg = colors.darkblack },
     },
     insert = {
       a = { fg = colors.black, bg = colors.green, gui = "bold" },
-      b = { fg = colors.white, bg = colors.mauve },
-      c = { fg = colors.white, bg = colors.darkblack },
+      b = { fg = colors.purple, bg = colors.mauve },
+      c = { fg = colors.purple, bg = colors.darkblack },
     },
     visual = {
       a = { fg = colors.black, bg = colors.orange, gui = "bold" },
-      b = { fg = colors.white, bg = colors.mauve },
-      c = { fg = colors.white, bg = colors.darkblack },
+      b = { fg = colors.purple, bg = colors.mauve },
+      c = { fg = colors.purple, bg = colors.darkblack },
     },
     replace = {
       a = { fg = colors.black, bg = colors.coral, gui = "bold" },
-      b = { fg = colors.white, bg = colors.mauve },
-      c = { fg = colors.white, bg = colors.darkblack },
+      b = { fg = colors.purple, bg = colors.mauve },
+      c = { fg = colors.purple, bg = colors.darkblack },
     },
     command = {
       a = { fg = colors.black, bg = colors.cyan, gui = "bold" },
-      b = { fg = colors.white, bg = colors.mauve },
-      c = { fg = colors.white, bg = colors.darkblack },
+      b = { fg = colors.purple, bg = colors.mauve },
+      c = { fg = colors.purple, bg = colors.darkblack },
     },
     inactive = {
-      a = { fg = colors.white, bg = colors.darkblack, gui = "bold" },
-      b = { fg = colors.white, bg = colors.darkblack },
-      c = { fg = colors.white, bg = colors.darkblack },
+      a = { fg = colors.purple, bg = colors.darkblack, gui = "bold" },
+      b = { fg = colors.purple, bg = colors.darkblack },
+      c = { fg = colors.purple, bg = colors.darkblack },
     },
   }
   return zitchdog
