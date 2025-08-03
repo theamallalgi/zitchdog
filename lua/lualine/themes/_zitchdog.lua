1812c39ba8be5b3207bc9172eb6d630572f3ae35
Amal Lalgi <amallalgi2487@gmail.com>
2025-08-03 17:49:52 +0530

    test: what the fuuuuck

---
+++ lua/lualine/themes/_zitchdog.lua
@@ -0,0 +1,45 @@
+local M = {}
+
+local function get_palette(variant)
+  local palettes = require("zitchdog.common.palette").variants
+  return palettes[variant]
+end
+
+function M.get(variant)
+  local colors = get_palette(variant)
+  local zitchdog = {
+    normal = {
+      a = { fg = colors.black, bg = colors.purple, gui = "bold" },
+      b = { fg = colors.white, bg = colors.mauve },
+      c = { fg = colors.white, bg = colors.ash },
+    },
+    insert = {
+      a = { fg = colors.black, bg = colors.green, gui = "bold" },
+      b = { fg = colors.white, bg = colors.mauve },
+      c = { fg = colors.white, bg = colors.ash },
+    },
+    visual = {
+      a = { fg = colors.black, bg = colors.orange, gui = "bold" },
+      b = { fg = colors.white, bg = colors.mauve },
+      c = { fg = colors.white, bg = colors.ash },
+    },
+    replace = {
+      a = { fg = colors.black, bg = colors.coral, gui = "bold" },
+      b = { fg = colors.white, bg = colors.mauve },
+      c = { fg = colors.white, bg = colors.ash },
+    },
+    command = {
+      a = { fg = colors.black, bg = colors.cyan, gui = "bold" },
+      b = { fg = colors.white, bg = aumauve },
+      c = { fg = colors.white, bg = colors.ash },
+    },
+    inactive = {
+      a = { fg = colors.white, bg = colors.ash, gui = "bold" },
+      b = { fg = colors.white, bg = colors.ash },
+      c = { fg = colors.white, bg = colors.ash },
+    },
+  }
+  return zitchdog
+end
+
+return M
---
+++ lua/lualine/themes/zitchdog.lua
@@ -0,0 +1,3 @@
+local config = require("zitchdog.config")
+
+return require("lualine.themes._zitchdog").get(config.options.variant)
