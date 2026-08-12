8d3e486e3ad89c8afc9eeb8d902cc404c6fb2772
Amal Lalgi <amallalgi2487@gmail.com>
2026-08-12 19:39:19 +0530

    feat(nvim/plugins): add support for `HiPhish/rainbow-delimiters.nvim"`

---
+++ lua/zitchdog/groups/plugins/rainbow.lua
@@ -0,0 +1,18 @@
+local M = {}
+
+M.url = "https://github.com/HiPhish/rainbow-delimiters.nvim"
+
+function M.create(palette, zitch_pattern, config)
+	local groups = {
+		RainbowDelimiterRed = { fg = palette.red },
+		RainbowDelimiterOrange = { fg = palette.orange },
+		RainbowDelimiterYellow = { fg = palette.yellow },
+		RainbowDelimiterGreen = { fg = palette.green },
+		RainbowDelimiterBlue = { fg = palette.blue },
+		RainbowDelimiterViolet = { fg = palette.purple },
+		RainbowDelimiterCyan = { fg = palette.cyan },
+	}
+	return groups
+end
+
+return M
