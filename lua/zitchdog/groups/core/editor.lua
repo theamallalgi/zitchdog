340cd9e54c81d710690751c7c720a4b6d74c7ded
Amal Lalgi <amallalgi2487@gmail.com>
2026-08-13 18:09:51 +0530

    fix: replace mistyped `fg` with `bg`

--- lua/zitchdog/groups/core/editor.lua
+++ lua/zitchdog/groups/core/editor.lua
@@ -4,7 +4,7 @@ function M.create(palette, zitch_pattern, config)
 	local bg = config.transparent_bg and "NONE" or palette.black
 	local darkBg = config.transparent_bg and "NONE" or palette.darkblack
 	local groups = {
-		CursorLine = { fg = palette.lightblack },
+		CursorLine = { bg = palette.lightblack },
 		-- CursorLine = { bg = config.transparent_bg and "NONE" or palette.charcoal },
 		Cursor = { bg = palette.magenta },
 		lCursor = { bg = palette.magenta },
