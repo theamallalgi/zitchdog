c3991141bd1e72ec83a656b467cd941b78d26763
Amal Lalgi <amallalgi2487@gmail.com>
2026-08-17 23:52:00 +0530

    feat(nvim/plugins): add better colors for `nvim-lualine/lualine.nvim`

--- lua/zitchdog/config.lua
+++ lua/zitchdog/config.lua
@@ -6,9 +6,9 @@ local M = {}
 ---| "dune"
 
 ---@class ZitchdogConfig
+---@field variant "grape"|"pine" the default colorscheme variant
 ---@field transparent_bg boolean
 ---@field variant ZitchdogVariant
----@field variant "grape"|"pine" the default colorscheme variant
 ---@field italic_comments boolean
 ---@field fg string?
 ---@field bg string?
@@ -16,9 +16,9 @@ local M = {}
 
 ---@type ZitchdogConfig
 M.options = {
-	transparent_bg = false, -- boolean, toggles a transparent background
-	disable_cursorline = false, -- boolean, hides CursorLine highlight by matching it to bg
 	variant = "grape", -- the default colorscheme variant
+	transparent_bg = false, -- boolean, toggles a transparent background
+	disable_cursorline = true, -- boolean, hides CursorLine highlight by matching it to bg
 	italic_comments = false, -- boolean, toggles italic comments
 	fg = nil, -- override the base foreground (defaults to the variant's white)
 	bg = nil, -- override the base background (defaults to the variant's black; ignored when transparent_bg = true)
