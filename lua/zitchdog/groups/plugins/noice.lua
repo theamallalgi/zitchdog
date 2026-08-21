0986e06aac35dfa5d950f4425b2e9a56f00a5985
Amal Lalgi <amallalgi2487@gmail.com>
2026-08-21 20:42:30 +0530

    chore(folke/noice): add better color support

--- lua/zitchdog/groups/plugins/noice.lua
+++ lua/zitchdog/groups/plugins/noice.lua
@@ -8,30 +8,35 @@ M.url = "https://github.com/folke/noice.nvim"
 ---@return ZitchdogHighlights
 function M.create(palette, zitch_pattern, config)
 	local groups = {
-		NoiceCmdlinePopupBorder = { fg = palette.green, bg = "NONE" },
-		NoiceCmdlineIcon = { fg = palette.green, bg = "NONE" },
-		NoiceCmdlinePopupBorderCalculator = { fg = palette.green, bg = "NONE" },
-		NoiceCmdlineIconCalculator = { fg = palette.green, bg = "NONE" },
-		NoiceCmdlinePopupBorderCmdline = { fg = palette.green, bg = "NONE" },
-		NoiceCmdlineIconCmdline = { fg = palette.green, bg = "NONE" },
-		NoiceCmdlinePopupBorderFilter = { fg = palette.green, bg = "NONE" },
-		NoiceCmdlineIconFilter = { fg = palette.green, bg = "NONE" },
-		NoiceCmdlinePopupBorderHelp = { fg = palette.yellow, bg = "NONE" },
-		NoiceCmdlineIconHelp = { fg = palette.yellow, bg = "NONE" },
-		NoiceCmdlinePopupBorderIncRename = { fg = palette.blue, bg = "NONE" },
-		NoiceCmdlineIconIncRename = { fg = palette.blue, bg = "NONE" },
-		NoiceCmdlinePopupBorderInput = { fg = palette.green, bg = "NONE" },
-		NoiceCmdlineIconInput = { fg = palette.green, bg = "NONE" },
-		NoiceCmdlinePopupBorderLua = { fg = palette.green, bg = "NONE" },
-		NoiceCmdlineIconLua = { fg = palette.green, bg = "NONE" },
-		NoiceCmdlinePopupBorderSearch = { fg = palette.orange, bg = "NONE" },
-		NoiceCmdlineIconSearch = { fg = palette.orange, bg = "NONE" },
 		-- lsp progress
 		NoiceLspProgressTitle = { fg = palette.magenta, bg = "NONE", bold = true },
 		NoiceLspProgressClient = { fg = palette.green, bg = "NONE", bold = true },
 		NoiceLspProgressSpinner = { fg = palette.blue, bg = "NONE", bold = true },
 		NoiceFormatProgressDone = { fg = palette.clay, bg = palette.yellow, bold = true },
 		NoiceFormatProgressTodo = { fg = palette.yellow, bg = palette.clay, bold = true },
+    -- cmdline
+    NoiceCmdline = { fg = palette.purple },
+    NoiceCmdlinePopupBorderCmdline = { fg = palette.purple },
+    NoiceCmdlinePopupBorder = { fg = palette.slate },
+    NoiceCmdlinePopupTitle = { fg = palette.green },
+    NoicePopupmenuMatch = { fg = palette.green, bg = palette.pine, bold = true },
+    NoiceCmdlinePopupBorderSearch = { fg = palette.orange },
+    NoiceCmdlinePopupBorderFilter = { fg = palette.blue },
+    NoiceCmdlinePopupBorderLua = { fg = palette.blue },
+    NoiceCmdlinePopupBorderHelp = { fg = palette.yellow },
+    NoiceCmdlinePopupBorderInput = { fg = palette.blue },
+    NoiceCmdlinePopupBorderCalculator = { fg = palette.cyan },
+    NoiceCmdlinePopupBorderIncRename = { fg = palette.yellow },
+    NoiceCmdlineIcon = { fg = palette.cyan, bg = palette.black },
+    NoiceCmdlineIconCmdline = { fg = palette.slate },
+    NoiceCmdlineIconSearch = { fg = palette.orange },
+    NoiceCmdlineIconFilter = { fg = palette.blue },
+    NoiceCmdlineIconLua = { fg = palette.blue },
+    NoiceCmdlineIconHelp = { fg = palette.yellow },
+    NoiceCmdlineIconInput = { fg = palette.blue },
+    NoiceCmdlineIconCalculator = { fg = palette.cyan },
+    NoiceCmdlineIconIncRename = { fg = palette.yellow },
+
 	}
 	return groups
 end
