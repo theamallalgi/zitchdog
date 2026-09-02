e215941ed49c4f4afb753dce9108ad43952ff804
Amal Lalgi <amallalgi2487@gmail.com>
2026-09-03 01:01:14 +0530

    feat: add proper colors for `COMMIT_EDITMSG`

--- lua/zitchdog/groups/plugins/git.lua
+++ lua/zitchdog/groups/plugins/git.lua
@@ -8,6 +8,17 @@ M.url = "https://github.com/lewis6991/gitsigns.nvim"
 ---@return ZitchdogHighlights
 function M.create(palette, zitch_pattern, config)
 	local groups = {
+		-- native git buffer stuff (try: git commit --verbose)
+		diffAdded = { fg = palette.green, bg = palette.pine },
+		diffRemoved = { fg = palette.red, bg = palette.maroon },
+		diffChanged = { fg = palette.blue, bg = palette.indigo },
+		diffFile = { fg = "#c0c9c3", bg = "#051177", bold = false },
+		diffIndexLine = { fg = palette.pink },
+		diffOldFile = { fg = palette.orange },
+		diffNewFile = { fg = palette.green },
+		diffLine = { fg = palette.purple },
+		gitcommitDiff = { fg = palette.slate },
+
 		-- gitsigns.nvim: see https://github.com/lewis6991/gitsigns.nvim
 		GitSignsAddLn = { fg = "NONE", bg = "#091511" },
 		GitSignsChangeLn = { fg = "NONE", bg = "#121429" },
